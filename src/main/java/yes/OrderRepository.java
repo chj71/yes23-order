0be55ded4d8ea372580ef0ae5bd22c6e5833a540
chj71 <chj7171@gmail.com>
2020-11-04 01:56:35 +0900

    3rd commit

--- src/main/java/yes/OrderRepository.java
+++ src/main/java/yes/OrderRepository.java
@@ -2,7 +2,7 @@ package yes;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
 
 
 }
